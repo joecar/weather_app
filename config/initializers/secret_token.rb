721811e13d8aa577b0a7efcebacd989ac9158eea
Joe Cartano <joe@psmacbook-001.local>
2013-03-10 15:48:28 -0700

    initial commit for what will become a simple skiing weather app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherApp::Application.config.secret_token = '77efde2489d0fe752ecb11d5c9607b00f8a953e56937d0d3745f4d7da143c0f746bbe47edfa16dcedc54cfbbb98aac1441c16d739a0bb0876e5623d3b436399a'
