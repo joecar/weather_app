62634cae6a5d69c24e004a4b9c96acc7f80b2bdb
Joe Cartano <joe@psmacbook-001.local>
2013-03-11 21:39:30 -0700

    render weather nicely, some clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 WeatherApp::Application.routes.draw do
+  root :to => 'weather#home'
+  get "weather/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
